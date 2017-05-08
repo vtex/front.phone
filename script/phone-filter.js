f1fe6c6917eeb42ae4fbe546f64ca40d422ae4f2
Breno Calazans <brenocalazans@gmail.com>
2017-05-08 18:50:08 -0300

    Updates

--- script/phone-filter.js
+++ script/phone-filter.js
@@ -1,2 +1,2 @@
-/*! front-phone - v4.4.0 - https://vtex.github.io/front.phone/ */
+/*! front-phone - v4.4.1 - https://vtex.github.io/front.phone/ */
 !function(e){function n(o){if(t[o])return t[o].exports;var r=t[o]={exports:{},id:o,loaded:!1};return e[o].call(r.exports,r,r.exports,n),r.loaded=!0,r.exports}var t={};return n.m=e,n.c=t,n.p="/@vtex/phone/script/",n(0)}([function(e,n,t){window.angular.module("vtex.phoneFilter",[]).filter("phone",["$window",function(e){return function(n,t,o){var r;return n?(o&&(r=e.vtex.phone.getPhoneNational(n,o)),r&&o||(r=e.vtex.phone.getPhoneInternational(n)),t=t?e.vtex.phone[t.toUpperCase()]:e.vtex.phone.INTERNATIONAL,e.vtex.phone.format(r,t)):"N/A"}}])}]);
